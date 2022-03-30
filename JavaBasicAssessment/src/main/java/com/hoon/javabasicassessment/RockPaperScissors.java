478647b5f4eca80197664a754c2de295597e390f
Jeonghoon Lee <hoon22.lee@gmail.com>
2022-03-30 13:50:47 -0400

    created java project for assessment about java basic

---
+++ JavaBasicAssessment/src/main/java/com/hoon/javabasicassessment/RockPaperScissors.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.hoon.javabasicassessment;
+
+/**
+ *
+ * @author Jeonghoon
+ */
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
